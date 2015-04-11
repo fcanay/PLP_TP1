6396f96f4999e98c8ed1e8e02a10c3e909f5b69a
Santi <santiaboy2@hotmail.com>
2015-04-11 19:39:31 -0300

    agregue un par de comentarios. Arregle typo. Sigue tirando lo del constructor G

--- Lomoba.hs
+++ Lomoba.hs
@@ -10,7 +10,7 @@ import Tipos
 foldExp fVar fNot fOr fAnd fD fB ex = case ex of  
                                                   Var p         -> fVar p
                                                   Not exr       -> fNot rec exr
-                                                  Or  exr1 exr2 -> fOr  (rec exr1) (rec exr2) 
+                                                  Or  exr1 exr2 -> fOr (rec exr1) (rec exr2) 
                                                   And exr1 exr2 -> fAnd (rec exr1) (rec exr2)
                                                   D   exr1      -> fD rec exr1
                                                   B   exr1      -> fB rec exr1
@@ -27,6 +27,7 @@ extraer = foldExp (id:[]) id union union id id
 eval :: Modelo -> Mundo -> Exp -> Bool
 eval mod m exp = eval' mod exp m
 
+--Santi: Por que el 'm' (de mundo) no lo pones a la izq y lo pedis en todas las lambdas?
 eval':: Modelo -> Exp -> Mundo -> Bool
 eval' (K (G ns f) fProp) exp = foldExp  (\p m -> elem m (fProp p))  
                                         (\f1 -> (\m -> not(f1 m)))  
@@ -47,8 +48,9 @@ valeEn exp (K (G ns f) fProp) = [ n | n <- ns, eval m n exp ]
 quitar :: Exp -> Modelo -> Modelo
 quitar exp m = foldr quitarMundo m (valeEn exp m)
 
+--Santi: Quisiste poner 'sacarNodo n g'?
 quitarMundo :: Mundo -> Modelo -> Modelo
-quitarMundo n (K g fProp) = K (sacarNodo g) (\p -> filter (!=n) (fProp p))
+quitarMundo n (K g fProp) = K (sacarNodo g) (\p -> filter (/=n) (fProp p))
 
 -- Ejercicio 16
 cierto :: Modelo -> Exp -> Bool
